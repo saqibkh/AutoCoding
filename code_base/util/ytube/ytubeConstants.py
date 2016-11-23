0f18fc20abaeac7e38fa62d70b92e7515e8d7dc5
saqibkh <saqibkhan@utexas.edu>
2016-11-23 12:22:14 -0600

    Update ytubeConstants.py

--- code_base/util/ytube/ytubeConstants.py
+++ code_base/util/ytube/ytubeConstants.py
@@ -2,7 +2,6 @@
 
 class ytubeConstants(object):
     URLS = ["https://www.youtube.com/watch?v=6KBgmakci9Q",
-            "https://www.youtube.com/watch?v=3guN_CyRp3g",
             "https://www.youtube.com/watch?v=3guN_CyRp3g"]
 
     def __init__(self):
