1868cb7e84c0cfb95678cff1b14b76d8b3328ef4
saqibkh <saqibkhan@utexas.edu>
2016-12-16 11:55:56 -0600

    Update ytubeConstants.py

--- code_base/util/ytube/ytubeConstants.py
+++ code_base/util/ytube/ytubeConstants.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python
 
 class ytubeConstants(object):
-    URLS = ["https://www.youtube.com/watch?v=3guN_CyRp3g",
-           "https://www.youtube.com/watch?v=6KBgmakci9Q"]
 
     URLS = [["https://www.youtube.com/watch?v=3guN_CyRp3g" ,3],
             ["https://www.youtube.com/watch?v=6KBgmakci9Q" , 4]]
