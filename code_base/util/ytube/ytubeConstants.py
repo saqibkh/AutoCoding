16ab139bc88797f26a9c9bad7c2bd8cdfdacb4ae
saqibkh <saqibkhan@utexas.edu>
2016-12-21 09:39:14 -0600

    Update ytubeConstants.py

--- code_base/util/ytube/ytubeConstants.py
+++ code_base/util/ytube/ytubeConstants.py
@@ -4,6 +4,9 @@ class ytubeConstants(object):
 
     URLS = [["https://www.youtube.com/watch?v=Og4J1oM7qjE" ,3],
             ["https://www.youtube.com/watch?v=6KBgmakci9Q" , 1]]
+    
+    # emailaddress, emailpassword, FirstName, LastName, Country
+    ACCOUNTS = [["skhansa00001@gmail.com", "", "sal", "khan", "Pakistan"]]
 
     def __init__(self):
         ##
