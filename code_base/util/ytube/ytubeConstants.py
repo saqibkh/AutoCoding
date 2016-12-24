4fdd4d5a2dd99c902d8e7d993ba7d6ac0a42451a
saqibkh <saqibkhan@utexas.edu>
2016-12-24 06:05:58 -0600

    Update ytubeConstants.py

--- code_base/util/ytube/ytubeConstants.py
+++ code_base/util/ytube/ytubeConstants.py
@@ -3,7 +3,8 @@
 class ytubeConstants(object):
 
     URLS = [["https://www.youtube.com/watch?v=Og4J1oM7qjE" ,3],
-            ["https://www.youtube.com/watch?v=9gGSjsXb52g", 7]]
+            ["https://www.youtube.com/watch?v=9gGSjsXb52g", 7],
+            ["https://www.youtube.com/watch?v=UsBGMZSEm0E", 2]]
     
     # emailaddress, emailpassword, FirstName, LastName, Country
     # "skhansa00001@gmail.com", "", "sal", "khan", "Pakistan"
